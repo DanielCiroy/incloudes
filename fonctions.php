d78877e31642f42a100ffe6e699a281a182f6814
CHIROY <danielciroy87@gmail.com>
2023-11-03 21:57:02 +0200

    ajout de l'exo incloudes ou insertion sur github

---
+++ fonctions.php
@@ -0,0 +1,36 @@
+<?php
+// functions.php
+
+function isValidRecipe(array $recipe) : bool
+{
+    if (array_key_exists('is_enabled', $recipe)) {
+        $isEnabled = $recipe['is_enabled'];
+    } else {
+        $isEnabled = false;
+    }
+
+    return $isEnabled;
+}
+
+function displayAuthor(string $authorEmail, array $users) : string
+{
+    for ($i = 0; $i < count($users); $i++) {
+        $author = $users[$i];
+        if ($authorEmail === $author['email']) {
+            return $author['full_name'] . '(' . $author['age'] . ' ans)';
+        }
+    }
+}
+
+function getRecipes(array $recipes) : array
+{
+    $validRecipes = [];
+
+    foreach($recipes as $recipe) {
+        if (isValidRecipe($recipe)) {
+            $validRecipes[] = $recipe;
+        }
+    }
+
+    return $validRecipes;
+}
---
+++ index.php
@@ -0,0 +1,41 @@
+<!-- index.php -->
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Site de recettes - Page d'accueil</title>
+    <link
+        href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" 
+        rel="stylesheet"
+    >
+</head>
+<body class="d-flex flex-column min-vh-100">
+    <div class="container">
+
+    <?php include_once('header.php'); ?>
+        <h1>Site de recettes</h1>
+
+        <!-- inclusion des variables et fonctions -->
+        <?php
+            include_once('variables.php');
+            include_once('fonctions.php');
+        ?>
+
+        <!-- inclusion de l'entête du site -->
+        <?php include_once('header.php'); ?>
+        
+        <?php foreach(getRecipes($recipes) as $recipe) : ?>
+            <article>
+                <h3><?php echo $recipe['title']; ?></h3>
+                <div><?php echo $recipe['recipe']; ?></div>
+                <i><?php echo displayAuthor($recipe['author'], $users); ?></i>
+            </article>
+        <?php endforeach ?>
+    </div>
+
+    <!-- inclusion du bas de page du site -->
+    <?php include_once('footer.php'); ?>
+</body>
+</html>
